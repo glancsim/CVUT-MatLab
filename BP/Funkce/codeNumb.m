60f80bc178b81a2e03599fd1c978602062f26110
glancsim <simon.glanc@seznam.cz>
2023-05-02 13:26:16 +0200

    example 5 BP 3D + 2D

--- BP/Funkce/codeNumb.m
+++ BP/Funkce/codeNumb.m
@@ -1,7 +1,7 @@
 function codeNumbers = codeNumb (beam,nodes)
     p = 0 ;
     for n = 1:nodes.nnodes                
-        for k = 1:nodes.ndofs
+        for k = 1:nodes.dimdofs
             if nodes.dofs(n,k) == 1
                 nodes.codeDofs(n,k) = p+1;
                 p = p+1;
@@ -11,8 +11,8 @@ function codeNumbers = codeNumb (beam,nodes)
         end
     end
     for b = 1:beam.nbeam
-        codeNumbers(b,1:nodes.ndofs) = nodes.codeDofs(beam.nodes1(b),:)
-        codeNumbers(b,nodes.ndofs+1:2*nodes.ndofs) = nodes.codeDofs(beam.nodes2(b),:)        
+        codeNumbers(b,1:nodes.dimdofs) = nodes.codeDofs(beam.nodes1(b),:);
+        codeNumbers(b,nodes.dimdofs+1:2*nodes.dimdofs) = nodes.codeDofs(beam.nodes2(b),:)    ;    
     end
     
 end
