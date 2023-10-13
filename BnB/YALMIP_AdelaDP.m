e20ab7e318859eda8279b68e68d4165512fb50e8
Simon Glanc <simon.glanc@histruct.com>
2023-10-13 10:35:53 +0200

    rename the file

--- BnB/YALMIP_AdelaDP.m
+++ BnB/YALMIP_AdelaDP.m
@@ -3,7 +3,8 @@ clear
 % It's good practice to start by clearing YALMIPs internal database 
 % Every time you call sdpvar etc, an internal database grows larger
 yalmip('clear')
- 
+
+ profile on
 % Define variables
 x = intvar(1,1);
 y = intvar(1,1);
@@ -13,13 +14,14 @@ Constraints = [-20*x - 10*y + 75 <= 0   ,...
                 12*x +  7*y - 55 <= 0   ,...
                 25*x + 10*y - 90 <= 0   ,...
                 0 <= x <= 3             ,...
-                0 <= x <= 7             ];
+                0 <= x <= 6             ];
 
 % Define an objective
 Objective = -20*x -10*y;
 
 % Set some options for YALMIP and solver
 options = sdpsettings('solver','gurobi');
+options.gurobi.Method = 1;
 
 % Solve the problem
 sol = optimize(Constraints,Objective,options);
@@ -34,3 +36,6 @@ else
  sol.info
  yalmiperror(sol.problem)
 end
+
+profile off
+profile viewer
