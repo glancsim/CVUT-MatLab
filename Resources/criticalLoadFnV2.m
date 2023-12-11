4abe2b9c99e20b8cd1ed74b75d06de61c8b8e64d
Simon Glanc <simon.glanc@histruct.com>
2023-12-11 01:57:59 +0100

    Create criticalLoadFnV2.m
    faster

---
+++ Resources/criticalLoadFnV2.m
@@ -0,0 +1,21 @@
+% Výpočet kritického břemene z matice tuhos ti a matice počátečních napětí
+%
+% In: 
+%   geometricMatrix.global =   globální matice počátečních napětí       
+%   stiffnesMatrix.global =   globální matice tuhosti       
+% Out:
+%   eigenVectors =      vlastní tvar konstrukce 
+%   eigeinValues =      vlastní čísla konstrukce
+%   criticalLoad =      kritické břemeno
+% 
+% (c) S. Glanc, 2022
+function [sorted_values]=criticalLoadFn(stiffnesMatrix,geometricMatrix)
+    values=eigs(stiffnesMatrix.global,-geometricMatrix.global,5,'smallestabs');
+    [~, index] = sort(abs(values));
+    sorted_values = values(index);
+end
+
+
+
+
+            
